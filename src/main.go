ec074e2d4db60024e83874290416d0c073163b2d
Milosz Galazka <m.galazkamilosz@gmail.com>
2022-01-25 23:59:31 +0100

    display char lore

--- src/main.go
+++ src/main.go
@@ -1,41 +1,60 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"time"
+	"strings"
 
 	"github.com/KnutZuidema/golio"
 	"github.com/KnutZuidema/golio/api"
-	"github.com/galazkamilosz/itemizer/src/model"
-	"github.com/go-bongo/bongo"
 	"github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	ApiKey           string `yaml:"key"`
-	ConnectionString string `yaml:"connectionString"`
-	Database         string `yaml:"database"`
+	ApiKey string `yaml:"key"`
+}
+
+type application struct {
+	riotClient *golio.Client
+}
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Itemizer"))
+}
+
+func (app *application) showChampion(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		w.WriteHeader(405)
+		w.Write([]byte("method not allowed"))
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		http.NotFound(w, r)
+		return
+	}
+	name = strings.Title(strings.ToLower(name))
+	fmt.Println(name)
+	champion, err := app.riotClient.DataDragon.GetChampion(name)
+	if err != nil {
+		log.Println(err)
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte(champion.Lore))
 }
 
 func main() {
 	config, err := getConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// mongo, err := connectToDatabase()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer mongo.Disconnect(context.Background())
-
-	bon, err := connectDb()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,17 +64,16 @@ func main() {
 		golio.WithRegion(api.RegionEuropeWest),
 		golio.WithLogger(logrus.New()),
 	)
-	champions, err := client.DataDragon.GetChampions()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// collection := mongo.Database("itemizer").Collection("champions")
-	for _, champion := range champions {
-		champ := model.Champion{
-			Champ: champion,
-		}
-		bon.Collection("champions").Save(&champ)
-	}
+
+	var app application
+	app.riotClient = client
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/champion", app.showChampion)
+
+	err = http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
 
 func getConfig() (*Config, error) {
@@ -71,32 +89,4 @@ func getConfig() (*Config, error) {
 		return nil, err
 	}
 	return config, nil
-}
-
-func connectToDatabase() (*mongo.Client, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		return nil, err
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		return nil, err
-	}
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
-
-func connectDb() (*bongo.Connection, error) {
-	connection, err := bongo.Connect(&bongo.Config{
-		ConnectionString: "localhost",
-		Database:         "itemizer",
-	})
-	if err != nil {
-		return nil, err
-	}
-	return connection, nil
 }
